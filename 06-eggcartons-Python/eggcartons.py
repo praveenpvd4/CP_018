d9d2b039f3d5fefa014c16196b9196b5fe790a96
praveenpvd4 <71496402+praveenpvd4@users.noreply.github.com>
2021-08-11 22:07:19 +0530

    praveen_018

--- 06-eggcartons-Python/eggcartons.py
+++ 06-eggcartons-Python/eggcartons.py
@@ -5,5 +5,17 @@
 
 
 def fun_eggcartons(eggs):
-	# your code goes here
-	return 1
+    	# your code goes here
+    box = 12
+    if(eggs == 0):
+        return 0
+    elif(1<=eggs<=box):
+        return 1
+    elif(12<=eggs<=box*2):
+         return 2
+    elif(24<=eggs<=box*3):
+        return 3
+    elif(36<=eggs<=box*4):
+        return 5
+    elif(48<=eggs<=box*5):
+         return 5
