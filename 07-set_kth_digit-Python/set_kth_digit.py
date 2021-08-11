7266834c610913809b2baa0a79ff697b9676dacd
praveenpvd4 <71496402+praveenpvd4@users.noreply.github.com>
2021-08-11 22:11:30 +0530

    praveen_018

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -6,6 +6,23 @@
 
 
 
-def fun_set_kth_digit(n, k, d):
-		return 0
+def getKthDigit(n, k):
+    n = abs(n)
+    if k == 0:
+        return n % 10
+    return getKthDigit(n // 10, k-1)
 
+def fun_set_kth_digit(n, k, d):
+    if n < 0:
+        output = fun_set_kth_digit(abs(n), k, d)
+        return -output
+    
+    check = n // (10 ** k)
+    
+    if check == 0:
+        result = d * (10 ** k) + n
+        return result
+    
+    else:
+        result = n + (d - getKthDigit(n, k)) * (10 ** k)
+        return result
