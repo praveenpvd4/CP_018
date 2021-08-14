07fce122f6784f1be933207db18937fcd123364f
praveenpvd4 <71496402+praveenpvd4@users.noreply.github.com>
2021-08-14 10:41:20 +0530

    ...

--- 13-isevenpositive-Python/isevenpositiveint.py
+++ 13-isevenpositive-Python/isevenpositiveint.py
@@ -5,5 +5,14 @@
 # than crashing), and isevenpositiveint(123456) returns True.
 
 def isevenpositiveint(x):
-	# your code goes here
-	pass
+    if type(x)==int:
+        if x>=0:
+            if x%2==0:
+                return True
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
+print(isevenpositiveint(2))
