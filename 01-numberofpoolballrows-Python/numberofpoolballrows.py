994148e0e3fd593a7f89070dacd8a7abdbcb65a0
praveenpvd4 <71496402+praveenpvd4@users.noreply.github.com>
2021-08-17 23:31:59 +0530

    ..

--- 01-numberofpoolballrows-Python/numberofpoolballrows.py
+++ 01-numberofpoolballrows-Python/numberofpoolballrows.py
@@ -5,6 +5,12 @@
 # returns 3. Note that if any balls must be in a row, then you count that row, and so 
 # numberOfPoolBallRows(7) returns 4 (since the 4th row must have a single ball in it).
 
-def fun_numberofpoolballrows(balls):
-	return 0
+def numberofpoolballs(rows):
+    	# Your code goes here
+	l=list()
+	for i in range(rows+1):
+		if i<=rows:
+			l.append(i)
+			s=sum(l)
+	return s
 
