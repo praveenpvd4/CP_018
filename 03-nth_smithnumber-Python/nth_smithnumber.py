88666389f20e383859d37f3717af5675bf9fc7d8
praveenpvd4 <71496402+praveenpvd4@users.noreply.github.com>
2021-09-02 23:19:02 +0530

    .

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -8,5 +8,52 @@
 # so fun_nthsmithnumber(1) should return 22
 
 
+def factors(n):
+    rt = []
+    f = 2
+    if n == 1:
+        rt.append(1)
+    else:
+        while 1:
+            if 0 == ( n % f ):
+                rt.append(f)
+                n //= f
+                if n == 1:
+                    return rt
+            else:
+                f += 1
+    return rt
+ 
+ 
+def sum_digits(n):
+    sum = 0
+    while n > 0:
+        m = n % 10
+        sum += m
+        n -= m
+        n //= 10
+ 
+    return sum
+ 
+ 
+def add_all_digits(lst):
+    sum = 0
+    for i in range (len(lst)):
+        sum += sum_digits(lst[i])
+ 
+    return sum
+
+def isSmith(i):
+    fac = factors(i)
+    if len(fac) > 1 and sum_digits(i) == add_all_digits(fac):
+        return True
+    return False
+
 def fun_nth_smithnumber(n):
-    return 1
+	count=0
+	i=1
+	while(count<=n):
+		if isSmith(i):
+			count+=1
+		i=i+1
+	return i-1
